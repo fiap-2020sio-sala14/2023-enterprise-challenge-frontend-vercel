fd3fb2a2587715097f07ae3caf04e1a719ccad08
Felipe Augusto Arantes de Souza <felipmoreira8@gmail.com>
2023-10-09 20:45:21 -0300

    Optimized conn module

--- libs/conn.js
+++ libs/conn.js
@@ -1,22 +1,26 @@
 import mongoose from 'mongoose';
-import tokens from "./tokens";
+import tokens from './tokens';
 
 const connectMongo = async () => {
     try {
-        const conn_creds = tokens.ITINERA_MONGODB_PUBLIC_CONN
-        const conn_string = (
-            conn_creds["STRING"].replace('${USER_ID}', conn_creds["USER_ID"])
-                                .replace('${USER_PASSWORD}', conn_creds["USER_PASSWORD"])
-                                .replace('${DB_NAME}', conn_creds["DB_NAME"])
-        );
+        const conn_creds = tokens.ITINERA_MONGODB_PUBLIC_CONN;
+        const conn_string = conn_creds.STRING
+                                      .replace('${USER_ID}', conn_creds.USER_ID)
+                                      .replace('${USER_PASSWORD}', conn_creds.USER_PASSWORD)
+                                      .replace('${DB_NAME}', conn_creds.DB_NAME);
         const { connection } = await mongoose.connect(conn_string);
 
-        if(connection.readyState == 1){
-            return Promise.resolve(true)
+        if (connection.readyState === 1) {
+            console.log('MongoDB connected successfully.');
+            return true;
+        } else {
+            console.error('MongoDB connection state is not 1.');
+            return false;
         }
     } catch (error) {
-        return Promise.reject(error)
+        console.error('Error connecting to MongoDB:', error);
+        throw error; // Rethrow the error for further handling, if needed.
     }
-}
+};
 
 export default connectMongo;
